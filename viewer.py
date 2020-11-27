2405f691b4a84cb075295829541b769522761703
Diogo <diogo.correia99@ua.pt>
2020-11-27 02:02:42 +0000

    created a function to draw tables and implemented it in main_loop

--- viewer.py
+++ viewer.py
@@ -12,7 +12,7 @@ import pygame
 from consts import RANKS, Tiles
 from mapa import Map
 from game import reduce_score
-from fetch import HighScoresFetch
+from scores import HighScoresFetch
 
 logging.basicConfig(level=logging.DEBUG)
 logger_websockets = logging.getLogger("websockets")
@@ -28,9 +28,14 @@ RIGHT_INFO_MARGIN_TOP = 30
 RIGHT_INFO_MARGIN_RIGHT = 30
 RIGHT_INFO_SPACE_BETWEEN_COLS = 5
 
+DATA_INDEX_BEST_ROUND = ["level", "timestamp", "timestamp2", "score", "total_moves", "total_pushes", "total_steps"]
+DATA_INDEX_CURR_ROUND = ["Moves", "Pushes", "Steps"]
+
 SCORE_INFO = {
     "level": "Level",
     "score": "Score",
+    "timestamp": "Date",
+    "timestamp2": "",
     "total_moves": "Moves",
     "total_pushes": "Pushes",
     "total_steps": "Steps"
@@ -57,6 +62,7 @@ SCALE = 1
 
 COLORS = {
     "white": (255, 255, 255),
+    "black": (0, 0, 0),
     "red": (255, 0, 0),
     "pink": (255, 105, 180),
     "blue": (135, 206, 235),
@@ -184,7 +190,7 @@ def draw_background(mapa):
     return background
 
 
-def draw_info(surface, text, pos, color=(0, 0, 0), background=None, size=24):
+def draw_info(surface, text, pos, color=COLORS["black"], background=None, size=24):
     """Creates text based surfaces for information display."""
     myfont = pygame.font.Font(None, int(size / SCALE))
     textsurface = myfont.render(text, True, color, background)
@@ -204,10 +210,39 @@ def draw_info(surface, text, pos, color=(0, 0, 0), background=None, size=24):
     surface.blit(textsurface, pos)
     return textsurface.get_width(), textsurface.get_height()
 
+
 # get size of draw without drawing it
 def get_draw_size(text):
-    textsurface = pygame.font.Font(None, int(24 / SCALE)).render(text, True, (0,0,0))
+    textsurface = pygame.font.Font(None, int(24 / SCALE)).render(text, True, COLORS["black"])
     return textsurface.get_width(), textsurface.get_height()
+
+# draw a table providing position for top and margin for right
+def draw_table_from_right_top(canvas, col_l_info, col_r_info, title_info, max_width, positions):
+    col_l, col_l_color = col_l_info
+    col_r, col_r_color = col_r_info
+    title, title_color = title_info
+    pos_top, margin_right = positions
+    canvas_width = canvas.get_width()
+
+    # find place for title
+    title_width, title_height = get_draw_size(title)
+    title_margin = (max_width-margin_right-title_width)/2
+    title_pos = title_width+title_margin+margin_right
+
+    # draw title of table
+    draw_info(canvas, title, (canvas_width-title_pos, pos_top), title_color)
+
+    content_initial_height = pos_top+title_height+20
+    for i, col in enumerate(col_l):
+        curr_height = content_initial_height+i*20
+        col_r_content = col_r[i]
+        
+        # draw left side of column
+        draw_info(canvas, col, (canvas_width-max_width, curr_height), col_l_color)
+
+        # draw right side of colum
+        draw_info(canvas, col_r_content, (canvas_width-get_draw_size(col_r_content)[0]-margin_right, curr_height), col_r_color)
+
 
 async def main_loop(queue):
     """Processes events from server and display's."""
@@ -243,9 +278,8 @@ async def main_loop(queue):
 
     new_event = True
 
-    last_player = state['player']
+    player = last_player = state['player']
 
-    data_index = ["level", "timestamp", "", "score", "total_moves", "total_pushes", "total_steps"]
     hs = HighScoresFetch(name=state['player'])
     best_entry = None
 
@@ -264,54 +298,56 @@ async def main_loop(queue):
         if "score" in state and "player" in state:
             if last_player != curr_player:
                 hs = HighScoresFetch(name=state['player'])
-                last_player = curr_player
-                best_entry = hs.get_best_entry(type="max", key="score")
+                if hs.data != []:
+                    best_entry = hs.get_best_entry(type="max", key="score")
 
-            player = state['player']
+                    split_timestamp = best_entry["timestamp"].split("T")
+                    fixed_width = get_draw_size(split_timestamp[0])[0]+get_draw_size("Data: ")[0]+RIGHT_INFO_MARGIN_RIGHT+RIGHT_INFO_SPACE_BETWEEN_COLS
+                    
+                    # adjust best_entry dict
+                    best_entry["timestamp"] = split_timestamp[0]
+                    best_entry["timestamp2"] = split_timestamp[1]
+
+                player = state['player']
+
+                last_player = curr_player
             
+            # draw player info
             player_h_from_top = SCREEN.get_height() - 40
             player_w, _ = get_draw_size(player)
             draw_info(SCREEN, player, (SCREEN.get_width()-player_w-RIGHT_INFO_MARGIN_RIGHT, player_h_from_top), COLORS["light_blue"])
             draw_info(SCREEN, "Player: ", (SCREEN.get_width()-player_w-get_draw_size("Player: ")[0]-RIGHT_INFO_MARGIN_RIGHT-RIGHT_INFO_SPACE_BETWEEN_COLS, player_h_from_top), COLORS["white"])
 
             if hs != None and hs.data != []:
-                draw_info(SCREEN, "Best Round", (SCREEN.get_width()-get_draw_size("Best Round")[0]-RIGHT_INFO_MARGIN_RIGHT-22, RIGHT_INFO_MARGIN_TOP), COLORS["yellow"])
-
-                info_pos = RIGHT_INFO_MARGIN_TOP + 35
-                title_fixed_size = get_draw_size(best_entry['timestamp'].split("T")[0])[0]+get_draw_size("Data: ")[0]+RIGHT_INFO_MARGIN_RIGHT+RIGHT_INFO_SPACE_BETWEEN_COLS
+                # table for best round
+                draw_table_from_right_top(SCREEN, ([SCORE_INFO[d] for d in DATA_INDEX_BEST_ROUND], COLORS["black"]), ([str(best_entry[info]) for info in DATA_INDEX_BEST_ROUND], COLORS["white"]), ("Best Round", COLORS["yellow"]), fixed_width, (RIGHT_INFO_MARGIN_TOP, RIGHT_INFO_MARGIN_RIGHT))
+            
+            # some conditions to coop with state['score']
+            if not isinstance(state['score'], int) and len(state['score']) > len(DATA_INDEX_CURR_ROUND):
+                # adapt position of current round table depending if there is a table for best round
+                current_round_pos_top = 230 if hs.data != [] else RIGHT_INFO_MARGIN_TOP
+                curr_round_data_fetch = [str(state['score'][i+1]) for i in range(len(DATA_INDEX_CURR_ROUND))]
                 
-                for i, info in enumerate(data_index):
-                    if info == "":
-                        continue
-                    else:
-                        content = best_entry[info]
-
-                    if info == "timestamp":
-                        splitted = content.split("T")
-                        draw_info(SCREEN, splitted[0], (SCREEN.get_width()-get_draw_size(splitted[0])[0]-RIGHT_INFO_MARGIN_RIGHT, info_pos+i*20), COLORS["white"])
-                        draw_info(SCREEN, "Data: ", (SCREEN.get_width()-title_fixed_size, info_pos+i*20))
-                        draw_info(SCREEN, splitted[1], (SCREEN.get_width()-get_draw_size(splitted[1])[0]-RIGHT_INFO_MARGIN_RIGHT, info_pos+(i+1)*20), COLORS["white"])
-                        continue              
-
-                    draw_info(SCREEN, str(content), (SCREEN.get_width()-get_draw_size(str(content))[0]-RIGHT_INFO_MARGIN_RIGHT, info_pos+i*20), COLORS["white"])
-                    draw_info(SCREEN, SCORE_INFO[info]+": ", (SCREEN.get_width()-title_fixed_size, info_pos+i*20))
-
-                curr_round_pos = 230
-                draw_info(SCREEN, "Current Round", (SCREEN.get_width()-get_draw_size("Current Round")[0]-RIGHT_INFO_MARGIN_RIGHT-10, curr_round_pos), COLORS["red"])
-
-                info_pos = curr_round_pos + 35
-                for i, curr_info in enumerate(["Moves", "Pushes", "Steps"]):
-                    content = state['score'][i+1]
-                    draw_info(SCREEN, str(content), (SCREEN.get_width()-get_draw_size(str(content))[0]-RIGHT_INFO_MARGIN_RIGHT, info_pos+i*20), COLORS["white"])
-                    draw_info(SCREEN, curr_info+": ", (SCREEN.get_width()-title_fixed_size, info_pos+i*20))
+                # if there is no data from best round, adapt the fixed size to the data of current round
+                if hs.data == []:
+                    max_key = lambda s:len(s)
+                    fixed_width = get_draw_size(max(curr_round_data_fetch, key=max_key))[0]+get_draw_size(max(DATA_INDEX_CURR_ROUND, key=max_key))[0]+RIGHT_INFO_MARGIN_RIGHT+RIGHT_INFO_SPACE_BETWEEN_COLS
+   
+                # table for current round
+                draw_table_from_right_top(SCREEN, (DATA_INDEX_CURR_ROUND, COLORS["black"]), (curr_round_data_fetch, COLORS["white"]), ("Current Round", COLORS["red"]), fixed_width, (current_round_pos_top, RIGHT_INFO_MARGIN_RIGHT))
 
         if "level" in state:
+            top_pos = 335 if hs.data != [] else 147
             draw_info(
                 SCREEN,
                 f"{state['level']}",
-                (SCREEN.get_width()-162, 335),
+                (SCREEN.get_width()-162, top_pos),
                 color=COLORS["white"], size=50
             )
+        else:
+            for i, word in enumerate(["Run a client  ", "to see scores!"]):
+                word_w, word_h = get_draw_size(word)
+                draw_info(SCREEN, word, (SCREEN.get_width()-word_w-40, RIGHT_INFO_MARGIN_TOP+word_h+i*20), COLORS["white"])
 
         if "boxes" in state:
             boxes_group.empty()
